2279100ffd891ffea6b1f301136d0cf0b469a714
phetzy <david.j.fetzer@gmail.com>
2024-05-06 20:25:05 -0400

    testing hello world with function arguments

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, World"
+	got := Hello("David")
+	want := "Hello, David"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
+
+/* Tests need to be in a file appending with "_test.go"
+   Test functions must start with word "Test"
+   Test function take one argument only "t *testing.T"
+   Testing needs to be imported similar to "fmt"
+   T in testing.T is the "hook"
+   can use T.Fail() when you want the end result as a fail
+   t.Errorf with print a message on failed test
+		f allows you to format your error message
+		%q used as placeholder for variables
+		for test %q is helpful as it wraps your values in double quotes ""
+*/
