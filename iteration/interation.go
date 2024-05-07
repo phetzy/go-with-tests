208d1484f99731191ec637da665ac1c3d777d778
phetzy <david.j.fetzer@gmail.com>
2024-05-07 03:08:04 -0400

    iterations
    included for loop for both pre 1.22 and post 1.22 for loops

---
+++ iteration/interation.go
@@ -0,0 +1,20 @@
+package iteration
+
+func Repeat(character string) string {
+	var repeated string
+
+	/* For loop pre 1.22
+
+	for i := 0; i < 5; i++ {
+		repeated += character
+	}
+	*/
+
+	// For loop 1.22 and later
+	for i := range 5 {
+		repeated += character
+		i++
+	}
+
+	return repeated
+}
