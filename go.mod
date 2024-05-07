3ac4341ff364c8ac0d885696b3537380c28a3132
phetzy <david.j.fetzer@gmail.com>
2024-05-07 01:21:46 -0400

    integers section
    integers secion of go with tests

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/phetzy/go-with-tests
+
+go 1.22.2
---
+++ integers/integers_test.go
@@ -0,0 +1,19 @@
+package integers
+
+import "testing"
+
+func TestIntegers(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
+}
+
+/*
+   '%d' is used to format integers instead of %q for strings
+
+
+
+*/
